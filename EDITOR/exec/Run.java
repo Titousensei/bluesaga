63c3ca035842b5a2ef2c1b3133cf1fd5352357a4
Eric Gaudet <egaudet@thefind.com>
2016-09-26 20:39:25 -0700

    Log editor console to file

--- EDITOR/exec/Run.java
+++ EDITOR/exec/Run.java
@@ -1,12 +1,14 @@
 import java.net.URLDecoder;
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Run
 {
   public static void main(String... args)
   throws Exception
   {
-    File runningJar = new File(Run.class.getProtectionDomain().getCodeSource().get‌Location().toURI());
+    File runningJar = new File(Run.class.getProtectionDomain().getCodeSource().getLocation().toURI());
     File runningDir = runningJar.getParentFile();
 
     String[] cmd = new String[] {
@@ -17,10 +19,13 @@ public class Run
         URLDecoder.decode(runningDir.getPath(), "UTF-8")
     };
 
+    String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+    File log = new File("editor-" + date + ".log");
     ProcessBuilder builder = new ProcessBuilder(cmd);
     builder.directory(runningDir.getParentFile());
     builder.redirectErrorStream(true);
-    builder.inheritIO();
+    builder.redirectOutput(ProcessBuilder.Redirect.appendTo(log));
+    //builder.inheritIO();
     Process p = builder.start();
     int ret = p.waitFor();
     System.exit(ret);

b2252a5fff290a583fdbc334716918c68afa1f5e
Eric Gaudet <egaudet@thefind.com>
2016-12-10 14:39:46 -0800

    shopbuilder can add individual items and abilities

--- SERVER/src/components/ShopBuilder.java
+++ SERVER/src/components/ShopBuilder.java
@@ -9,25 +9,67 @@ public class ShopBuilder
 extends Builder<Shop>
 {
   protected Shop s = null;
+  protected StringBuilder items = null;
+  protected List<Integer> abilities = null;
 
   public void init(int id, String name, String origin) {
     s = new Shop(id, name, origin);
   }
 
   public void items(String val) {
-    s.setItems(val);
+    if (items==null) {
+      items = new StringBuilder();
+    }
+    else {
+      items.append(',');
+    }
+    items.append(val);
+  }
+
+  public void item(String val) {
+    int id = parseInt(val);
+    if (id!=0) {
+      if (items==null) {
+        items = new StringBuilder(100);
+      }
+      else {
+        items.append(',');
+      }
+      items.append(id);
+    }
   }
 
   public void abilities(String val) {
     String abilityIds[] = val.split(",");
-    int[] ret = new int[abilityIds.length];
+    abilities = new ArrayList<>(abilityIds.length);
     for (int i = 0 ; i<abilityIds.length ; i++) {
-      ret[i] = Integer.parseInt(abilityIds[i]);
+      abilities.add(parseInt(abilityIds[i]));
     }
-    s.setAbilities(ret);
+  }
+
+  public void ability(String val) {
+    int id = parseInt(val);
+    if (id!=0) {
+      if (abilities==null) {
+        abilities = new ArrayList<>(10);
+      }
+      abilities.add(id);
+    }
   }
 
   public Shop build() {
+    if (items!=null) {
+      s.setItems(items.toString());
+    }
+    if (abilities!=null) {
+      int[] ab = new int[abilities.size()];
+      int i = 0;
+      for (Integer val : abilities) {
+        ab[i] = val;
+        ++ i;
+      }
+      s.setAbilities(ab);
+    }
     return s;
   }
 

2188503ac9ee09d24b1a9457fc2fea02093b7f62
Eric Gaudet <egaudet@thefind.com>
2017-01-01 23:55:01 -0800

    adding RecipeBuilder class, forgotten in earlier commit

---
+++ SERVER/src/data_handlers/crafting_handler/RecipeBuilder.java
@@ -0,0 +1,53 @@
+package data_handlers.crafting_handler;
+
+import java.util.*;
+
+import components.Builder;
+import data_handlers.item_handler.Item;
+import utils.ServerGameInfo;
+
+public class RecipeBuilder
+extends Builder<Recipe>
+{
+  protected Recipe r = null;
+
+  public void init(int id, String name, String origin) {
+    r = new Recipe(id, name, origin);
+  }
+
+  public void productId(String val) {
+//    r.setProductId(parseInt(val));
+  }
+
+  public void material(String val) {
+    Item material = ServerGameInfo.itemDef.get(parseInt(val));
+    r.addMaterial(material);
+  }
+
+  public void craftingStation(String val) {
+    r.setCraftingStation(Enum.valueOf(CraftingStation.class, val));
+  }
+
+  public void skillLevel(String val) {
+//    s.setSkillLevel(parseInt(val));
+  }
+
+  public Recipe build() {
+    return r;
+  }
+
+  public static void main(String... args) {
+    Map<Integer, Recipe> m = new HashMap<>();
+    Builder.load(args[0], RecipeBuilder.class, m);
+    if (args.length>1) {
+      if ("-".equals(args[1])) {
+        for (Recipe r : m.values()) {
+          System.out.println(r);
+        }
+      }
+      else {
+        System.out.println(m.get(parseInt(args[1])));
+      }
+    }
+  }
+}

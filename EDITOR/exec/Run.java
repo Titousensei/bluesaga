0f494477687b73969211d3154e3d2a93b9a94680
Eric Gaudet <egaudet@thefind.com>
2017-01-21 00:21:10 -0800

    Run.jar checks java version before running

--- EDITOR/exec/Run.java
+++ EDITOR/exec/Run.java
@@ -3,11 +3,26 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.swing.JOptionPane;
+
 public class Run
 {
+  public static final String MIN_VERSION = "1.8";
+
   public static void main(String... args)
   throws Exception
   {
+    String version = Runtime.class.getPackage().getImplementationVersion();
+
+    if (MIN_VERSION.compareTo(version) > 0) {
+      JOptionPane.showMessageDialog(null,
+          "This program requires Java " + MIN_VERSION
+              + " or above.\nPlease download the latest version at http://www.java.com/",
+          "Info: Java Version mismatch",
+          JOptionPane.WARNING_MESSAGE);
+      System.exit(-1);
+    }
+
     File runningJar = new File(Run.class.getProtectionDomain().getCodeSource().getLocation().toURI());
     File runningDir = runningJar.getParentFile();
 
